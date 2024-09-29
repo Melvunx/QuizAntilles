9bbcf47d5cc7deffbdc01a443a6abbaf04724d8f
Melvunx <augustine.melv@gmmail.com>
2024-09-29 15:01:58 +0200

    final setup questions

--- assets/data/Question.js
+++ assets/data/Question.js
@@ -18,120 +18,6 @@ class Question {
   }
 }
 
-const questionsUniques = [
-  {
-    question: " ",
-    choices: [
-      "Pour les fêtes",
-      "Pour les enterrement",
-      "Pour les soirées dansantes",
-    ],
-    correctAnswer: "Pour les enterrement",
-  },
-
-  {
-    question: "  ",
-    choices: [
-      "Une danse traditionnelle Martiniquaise",
-      " Opéra de Martinique",
-      " club de danse haïtien",
-    ],
-    correctAnswer: "Une danse traditionnelle Martiniquaise",
-  },
-
-  {
-    question: " ",
-    choices: ["Du piano", "Du triangle endiablé", "De la flûte en bambou"],
-    correctAnswer: "De la flûte en bambou",
-  },
-
-  {
-    question: " ",
-    choices: ["La Imperfecta", "La Perfecta", "Joris LARNOIR"],
-    correctAnswer: "La Perfecta",
-  },
-
-  {
-    question: " ",
-    choices: ["Kreyol la", "Zafem", "Les Aiglons", "Djet-X"],
-    correctAnswer: "Les Aiglons",
-  },
-
-  {
-    question: " ",
-    choices: ["Raï", "Bouyon", "Jamaïque"],
-    correctAnswer: "Raï",
-  },
-
-  {
-    question: " ",
-    choices: ["Fécat'Jy", "Edmonde Rusland", "Jid Kreyolad"],
-    correctAnswer: "Fécat'Jy",
-  },
-
-  {
-    question: " ",
-    choices: [" tour des Yoles", " Tchimbé Raid", "La savane des esclaves"],
-    correctAnswer: " tour des Yoles",
-  },
-
-  {
-    question: " ",
-    choices: ["22 Septembre 1920", "8 Mai 1902", "11 Novembre 1932"],
-    correctAnswer: "8 Mai 1902",
-  },
-
-  {
-    question: " ",
-    choices: ["Euzhan Palcy", "Aimé Césaire", "Frédéric Pichon"],
-    correctAnswer: "Euzhan Palcy",
-  },
-
-  {
-    question: " ",
-    choices: ["Joé dwet Filé", , "Gary Victor", "Alfred Marie Jeanne"],
-    correctAnswer: "Gary Victor",
-  },
-
-  {
-    question: " ",
-    choices: [
-      "L'abolition de l'esclavage",
-      " festival du crabe gras",
-      " carnaval",
-    ],
-    correctAnswer: " carnaval",
-  },
-
-  {
-    question: " ",
-    choices: [" phoque", " lamentin", "La Baleine à Bosse"],
-    correctAnswer: " lamentin",
-  },
-
-  {
-    question: " ",
-    choices: [
-      " poisson meurtrier",
-      " serpent venimeux",
-      " oiseau mangeur d'homme",
-    ],
-    correctAnswer: " serpent venimeux",
-  },
-
-  {
-    question: " ",
-    choices: [" Morne Rouge", " Lorrain", " Gros-Morne", "Saint-Pierre"],
-    correctAnswer: "Saint-Pierre",
-  },
-
-  {
-    question: " ",
-    choices: [" chien", " esprit maléfique", " plat de résistance"],
-    correctAnswer: " esprit maléfique",
-  },
-];
-
 export const questions = [
   new Question(
     "unique",
@@ -139,24 +25,28 @@ export const questions = [
     ["Morue", "Dorade", "Coulirou"],
     "Morue"
   ),
+
   new Question(
     "unique",
     "Qu'est ce que le féroce",
     ["Adjectif", "Boisson très alcoolisée", "Plat à base d'avocat"],
     "Plat à base d'avocat"
   ),
+
   new Question(
     "unique",
     "Qu'est qu'un bokit",
     ["Sandwich", "Kit de cuisine", "Animal"],
     "Sandwich"
   ),
+
   new Question(
     "unique",
     "Qu'est-ce qu'un caca boeuf",
     ["Excréments frits", "Fruit applatit", "Gâteau"],
     "Gâteau"
   ),
+
   new Question(
     "unique",
     "Qu'est-ce qu'un blaff",
@@ -167,6 +57,7 @@ export const questions = [
     ],
     "Plat de poisson"
   ),
+
   new Question(
     "unique",
     "Qu'est-ce qu'un dombré",
@@ -177,12 +68,14 @@ export const questions = [
     ],
     "Plat avec des boulettes de farine"
   ),
+
   new Question(
     "unique",
     "Le mancelinier est un arbre innoffensif",
     ["Oui", "Non", "On ne sait pas"],
     "Non"
   ),
+
   new Question(
     "unique",
     "Qu'est-ce que le «Chouval Bwa»",
@@ -193,73 +86,129 @@ export const questions = [
     ],
     "Style musical qui accompagne un manège"
   ),
+
   new Question(
     "unique",
     "Quand jouait-on habituellement du Gwoka en Guadeloupe",
-    [""],
-    ""
+    ["Pour les fêtes", "Pour les enterrement", "Pour les soirées dansantes"],
+    "Pour les enterrement"
   ),
+
   new Question(
     "unique",
     "Finissez cette phrase: «Zouk la sé sel ...»",
     ["Poivre et Girofle", "Medikamen nou ni", "Rimèd mwen ni"],
     "Medikamen nou ni"
   ),
-  new Question("unique", "Qu'est-ce que le «bèlè»", [""], ""),
+
+  new Question(
+    "unique",
+    "Qu'est-ce que le «bèlè»",
+    [
+      "Danse traditionnelle Martiniquaise",
+      "Opéra de Martinique",
+      "Club de danse haïtien",
+    ],
+    "Danse traditionnelle Martiniquaise"
+  ),
+
   new Question(
     "unique",
     "Quel est l'un des instruments joué le célèbre Eugène Mona",
-    [""],
-    ""
+    ["Piano", "Triangle endiablé", "Flûte en bambou"],
+    "De la flûte en bambou"
   ),
-  new Question("unique", "Qui interprète «La divinité»", [""], ""),
-  new Question("unique", "Quel groupe N'est PAS originaire d'Haiti", [""], ""),
+
   new Question(
     "unique",
-    "Parmis les musiques suivantes laquelle n'est PAS antillaise",
-    [""],
-    ""
+    "Qui interprète «La divinité»",
+    ["La Imperfecta", "La Perfecta", "Joris LARNOIR"],
+    "La Perfecta"
   ),
-  new Question("unique", "Qui chante «Sans bikini sans monokini...»", [""], ""),
+
+  new Question(
+    "unique",
+    "Quel groupe est originaire de la Guadeloupe",
+    ["Kreyol la", "Zafem", "Les Aiglons", "Djet-X"],
+    "Les Aiglons"
+  ),
+
+  new Question(
+    "unique",
+    "Parmis les musiques suivantes laquelle N'est PAS antillaise",
+    ["Raï", "Bouyon", "Jamaïque"],
+    "Raï"
+  ),
+
+  new Question(
+    "unique",
+    "Qui chante «Sans bikini sans monokini...»",
+    ["Fécat'Jy", "Edmonde Rusland", "Jid Kreyolad"],
+    "Fécat'Jy"
+  ),
+
   new Question(
     "unique",
     "Quelle compétion inscrite au patrimoine immatériel de l'Unesco fait la fierté des Martiniquais",
-    [""],
-    ""
+    ["Tour des Yoles", " Tchimbé Raid", "La savane des esclaves"],
+    "Tour des Yoles"
   ),
+
   new Question(
     "unique",
-    "Quand a eu lieu l'éruption volcanique de la Pelée la plus connue",
-    [""],
-    ""
+    "Quand a eu lieu la dernière éruption volcanique de la Montagne Pelée",
+    ["16 septembre 1929", "8 Mai 1902", "22 mai 1848"],
+    "16 septembre 1929"
   ),
-  new Question("unique", "Qui a réalisé «La Rue case nègre»", [""], ""),
+
+  new Question(
+    "unique",
+    "Qui a réalisé «La Rue case nègre»",
+    ["Euzhan Palcy", "Aimé Césaire", "Frédéric Pichon"],
+    "Euzhan Palcy"
+  ),
+
   new Question(
     "unique",
     "Qui a écrit «Le Diable dans un thé à la citronnelle»",
-    [""],
-    ""
+    ["Joé dwet Filé", , "Gary Victor", "Alfred Marie Jeanne"],
+    "Gary Victor"
   ),
+
   new Question(
     "unique",
     "Quel fête se déroule habituellement en février les jours gras",
-    [""],
-    ""
+    ["L'abolition de l'esclavage", "Festival du crabe gras", "Carnaval"],
+    "Carnaval"
   ),
+
   new Question(
     "unique",
     "Quelle communne de Martinique tire son nom d'un ancien animal marin qui vivait dans ses eaux",
-    [""],
-    ""
+    ["Phoque", "Lamentin", "Baleine à Bosse"],
+    "Lamentin"
   ),
-  new Question("unique", "Qu'es-ce que le Trigonocéphale", [""], ""),
+
   new Question(
     "unique",
-    "Quelle commune a été capitale de la Martinique",
-    [""],
-    ""
+    "Qu'es-ce que le Trigonocéphale",
+    ["Poisson meurtrier", "Serpent venimeux", "Oiseau mangeur d'homme"],
+    "Serpent venimeux"
   ),
-  new Question("unique", "Qu'est ce qu'un dowliss", [""], ""),
+
+  new Question(
+    "unique",
+    "Quelle commune a été capitale de la Martinique auparavant",
+    ["Morne Rouge", "Lorrain", "Gros-Morne", "Saint-Pierre"],
+    "Saint-Pierre"
+  ),
+
+  new Question(
+    "unique",
+    "Qu'est ce qu'un dowliss",
+    ["Chien", "Esprit maléfique", "Plat de résistance"],
+    "Esprit maléfique"
+  ),
 
   new Question(
     "multiple",
@@ -267,24 +216,28 @@ export const questions = [
     ["Portugaise", "Pakala", "Allemande", "Bleu"],
     ["Portugaise", "Pakala"]
   ),
+
   new Question(
     "multiple",
     "Que trouve-t-on dans un repas de Noël antillais",
     ["Pois d'angoles", "Jambon Nwel", "Pâtés salés", "Dinde", "Saumon fumé"],
     ["Pois d'angoles", "Jambon Nwel", "Pâtés salés"]
   ),
+
   new Question(
     "multiple",
     "Quels types de boudins N'existent PAS",
     ["Manmanw", "Rouge", "Vert", "Blanc", "Nègre"],
     ["Manmanw", "Vert", "Nègre"]
   ),
+
   new Question(
     "multiple",
     "Quels styles musicaux NE sont PAS originaires des antilles",
     ["Bigin", "Electro", "Kompa", "Mazurka"],
     ["Mazurka", "Electro"]
   ),
+
   new Question(
     "multiple",
     "Parmis ces capitales lesquelles N'en sont PAS",
