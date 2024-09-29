9b325b2aad023801a009bbd7a23215520c9c1fdb
Melvunx <augustine.melv@gmmail.com>
2024-09-29 10:58:28 +0200

    setup about

---
+++ app.js
@@ -0,0 +1,11 @@
+const popupMada = document.querySelector(".popup.mart");
+const popupGwada = document.querySelector(".popup.guad");
+
+madaPopup.addEventListener("click", () => (popupMada.style.display = "block"));
+gwadaPopup.addEventListener(
+  "click",
+  () => (popupGwada.style.display = "block")
+);
+
+closeMada.addEventListener("click", () => (popupMada.style.display = "none"));
+closeGwada.addEventListener("click", () => (popupGwada.style.display = "none"));
