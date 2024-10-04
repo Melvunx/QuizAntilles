ed8a2af87ceff123d8353e7dbadb4cfd47af30fb
Melvunx <augustine.melv@gmmail.com>
2024-10-04 14:05:48 +0200

    final setup about

--- assets/js/utils/Popup.js
+++ assets/js/utils/Popup.js
@@ -1,12 +1,12 @@
-const popupMada = document.querySelector(".popup.mart");
-const popupGwada = document.querySelector(".popup.guad");
+// const popupMada = document.querySelector(".popup.mart");
+// const popupGwada = document.querySelector(".popup.guad");
 
-madaPopup.addEventListener("click", () => (popupMada.style.display = "block"));
-gwadaPopup.addEventListener(
-  "click",
-  () => (popupGwada.style.display = "block")
-);
+// madaPopup.addEventListener("click", () => (popupMada.style.display = "block"));
+// gwadaPopup.addEventListener(
+//   "click",
+//   () => (popupGwada.style.display = "block")
+// );
 
-closeMada.addEventListener("click", () => (popupMada.style.display = "none"));
-closeGwada.addEventListener("click", () => (popupGwada.style.display = "none"));
+// closeMada.addEventListener("click", () => (popupMada.style.display = "none"));
+// closeGwada.addEventListener("click", () => (popupGwada.style.display = "none"));
 
